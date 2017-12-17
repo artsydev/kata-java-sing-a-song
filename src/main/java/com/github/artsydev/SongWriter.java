e7697f922c6745e5e3e9a9a737f3f8a81faeac59
Shane Thomas <shane120@gmail.com>
2017-12-17 13:46:43 -0700

    Refactored using flocking rules
    Used Sandi Metz's flocking rules to refactor into concise code
    eliminating many helper methods

--- src/main/java/com/github/artsydev/SongWriter.java
+++ src/main/java/com/github/artsydev/SongWriter.java
@@ -16,33 +16,27 @@ public class SongWriter {
         }
 
         if (cast.size() == 1) {
-            return firstVerse(cast.get(0).getName());
+            return swallowedLyric(cast.get(0), PERIOD);
         } else if (cast.size() == 2) {
-            return firstVerse(cast.get(0).getName()) + verseSeparator() + lastVerse(cast.get(1));
+            return swallowedLyric(cast.get(0), PERIOD) +
+                   verseSeparator() +
+                   swallowedLyric(cast.get(1), ELLIPSES);
         }
 
         return NO_LYRICS;
     }
 
     private static String verseSeparator() {
-        return "\n";
+        return "\n\n";
     }
 
-    private static String firstVerse(String animal) {
-        return swallowedLyric(animal, PERIOD) + dontKnowWhyLyric(animal);
+    private static String swallowedLyric(Animal animal, String punctuation) {
+        return String.format("There was an old lady who swallowed a %s%s\n%s",
+                             animal.getName(),
+                             punctuation,
+                             animal.getBehavior());
     }
 
-    private static String lastVerse(Animal animal) {
-        return swallowedLyric(animal.getName(), ELLIPSES) + animal.getBehavior();
-    }
-
-    private static String swallowedLyric(String animal, String punctuation) {
-        return String.format("There was an old lady who swallowed a %s%s\n", animal, punctuation);
-    }
-
-    private static String dontKnowWhyLyric(String animal) {
-        return String.format("I don't know why she swallowed a %s - perhaps she'll die!\n", animal);
-    }
 
     private static boolean noCastMembers(List<Animal> cast) {
         return cast == null || cast.size() == 0;
